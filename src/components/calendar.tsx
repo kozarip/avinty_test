fd0f263221d834cbf4266e6c6be143bbadc0204e
Peter Kozari <kozaripeti@gmail.com>
2022-11-18 11:47:03 +0100

    feat: appointments grid

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
-import { Grid } from '@mui/material';
-import "./calendar.scss"
+/* eslint-disable no-loop-func */
+import React, { useEffect, useState } from 'react';
 import moment from 'moment';
 import { AppointmentType, ConvertedAppointmentType } from '../utils/types';
+import "./calendar.scss"
+import { Modal, Button } from '@mui/material';
+import AppointmentDetails from './appointmentDetails';
 
 type CalendarProps = {
   date: any
@@ -10,66 +12,72 @@ type CalendarProps = {
 }
 
 const Calendar: React.FC<CalendarProps> = ({ date, appointments }) => {
+  const [isAppointmentModalOpen, setIsAppointmentModalOpen] = useState(false)
+  const [selectedAppointment, setSelectedAppointment] = useState<AppointmentType>(appointments[0])
+
   useEffect(() => {
+    console.log(appointments)
   }, [appointments])
 
-  const convertAppointmentsDate2Quarters = (rawAppointments: AppointmentType[]) => {
-    const convertedAppointments = rawAppointments.map((appointment: any) => {
-      const startQuarter = convertTimeToQuarters(appointment.start)
-      const endQuarter = convertTimeToQuarters(appointment.end)
-      appointment["startQuarter"] = startQuarter
-      appointment["endQuarter"] = endQuarter
-      return appointment
+  const renderAppointments = () => {
+    return appointments.map((appointment: AppointmentType, i) => {
+      const start = moment(appointment.start).format("HHmm")
+      const end = moment(appointment.end).format("HHmm")
+      return (
+        <div
+          className='calendarItem'
+          key={appointment.id}
+          style={{ gridRow: `time-${start} / time-${end}` }}
+          onClick={() => handleClickOnAppointment(appointment)}
+        >
+          <span>
+          {`${moment(appointment.start).format("HH:mm")} - ${moment(appointment.end).format("HH:mm")}`}
+          </span>
+          {appointment.title && <h4>{appointment.title}</h4>}
+        </div>
+      )
     })
-    console.log(convertedAppointments)
-    return convertedAppointments
   }
 
-  const convertTimeToQuarters = (time: any) => {
-    return (moment.duration(moment(time).format('HH:mm')).asMinutes() / 15)
-  }
-
-  const renderQuarters = () => {
-    let tr = [];
-    const convertedAppointments = convertAppointmentsDate2Quarters(appointments)
-    for (let i = 1; i <= 96; i++) {
-      const appointmentsInThisTime =
-        convertedAppointments.filter((convertedAppointment) => convertedAppointment.startQuarter === i)
-      let div = []
-      const isHour = i % 4 === 0
-      const appointmentsNumber = appointmentsInThisTime.length
-      if (appointmentsNumber === 0) {
-        tr.push(<div className={`empty ${isHour ? 'isHour' : ''}`}>{isHour && i / 4}</div>)
-      } else {
-        let reservedPlacesNumber = 0
-        div = appointmentsInThisTime.map((a: ConvertedAppointmentType) => {
-          const width = 100 / appointmentsNumber
-          const height =
-            a.endQuarter < a.startQuarter
-              ? 96 - a.startQuarter
-              : a.endQuarter - a.startQuarter
-          reservedPlacesNumber = height > 1 ? height : 0
-          //reservedPlacesNumber += appointmentsNumber;
-          return (
-            <div className='calendarItem' style={{
-                width: `${width}%`,
-                height: `${height * 30}px`
-              }}>
-              {`${moment(a.start).format('HH:mm')} - ${moment(a.end).format('HH:mm')}`}
-              {reservedPlacesNumber}
-            </div>
-          )
-        })
-        i += reservedPlacesNumber
-        tr.push(div)
-      }
-    }
-    return tr;
+  const handleClickOnAppointment = (appointment: AppointmentType) => {
+    setIsAppointmentModalOpen(true)
+    setSelectedAppointment(appointment)
   }
 
   return (
     <div className='calendarContainer'>
-      {renderQuarters()}
+      <div className='schedule'>
+        {/* <h5 style={{gridRow: "time-0800"}}>8:00</h5>
+        <h5 style={{gridRow: "time-0900"}}>9:00</h5>
+        <h5 style={{gridRow: "time-1000"}}>10:00</h5>
+        <h5 style={{gridRow: "time-1100"}}>11:00</h5>
+        <h5 style={{gridRow: "time-1200"}}>12:00</h5>
+        <h5 style={{gridRow: "time-1300"}}>13:00</h5>
+        <h5 style={{gridRow: "time-1400"}}>14:00</h5>
+        <h5 style={{gridRow: "time-1500"}}>15:00</h5>
+        <h5 style={{gridRow: "time-1600"}}>16:00</h5>
+        <h5 style={{gridRow: "time-1700"}}>17:00</h5>
+        <h5 style={{gridRow: "time-1800"}}>18:00</h5>
+        <h5 style={{gridRow: "time-1900"}}>19:00</h5>
+        <h5 style={{gridRow: "time-2000"}}>20:00</h5>
+        <h5 style={{gridRow: "time-2100"}}>21:00</h5>
+        <h5 style={{gridRow: "time-2200"}}>22:00</h5>
+        <h5 style={{gridRow: "time-2300"}}>23:00</h5> */}
+        {/* <h5 className='rowBorder' style={{gridRow: "time-0900"}}></h5> */}
+        {renderAppointments()}
+      </div>
+      <Modal
+        open={isAppointmentModalOpen}
+        onClose={() => setIsAppointmentModalOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <>
+          <AppointmentDetails
+            appointment={selectedAppointment}
+          />
+        </>
+      </Modal>
     </div>
   );
 };
